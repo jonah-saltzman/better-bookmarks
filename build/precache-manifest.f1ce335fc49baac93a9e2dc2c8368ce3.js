0702c6bad116b7e8a97957b4d176825792fe4f44
jonahsaltzman <jonahsaltzman@gmail.com>
2021-12-21 18:46:23 -0500

    add redirect to / for unknown pages

---
+++ build/precache-manifest.f1ce335fc49baac93a9e2dc2c8368ce3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc95f1d59b0698e676034af5cf543cb",
+    "url": "/better-bookmarks-client/index.html"
+  },
+  {
+    "revision": "cd335e6ec276795e95c9",
+    "url": "/better-bookmarks-client/static/css/2.a304e6d6.chunk.css"
+  },
+  {
+    "revision": "35820a9d1facbd5b61e4",
+    "url": "/better-bookmarks-client/static/css/main.b71574f3.chunk.css"
+  },
+  {
+    "revision": "cd335e6ec276795e95c9",
+    "url": "/better-bookmarks-client/static/js/2.4da158ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/better-bookmarks-client/static/js/2.4da158ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35820a9d1facbd5b61e4",
+    "url": "/better-bookmarks-client/static/js/main.9b002455.chunk.js"
+  },
+  {
+    "revision": "262bab9846ed6809d68d",
+    "url": "/better-bookmarks-client/static/js/runtime-main.a3ff2f4b.js"
+  }
+]);
--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,10 +1,9 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>404 page not found</h1>
-    </div>
+    <Redirect to='/'></Redirect>
   );
 };
 
