24f7d72ab89a0de38669721f23d81cd45b07f06a
jonahsaltzman <jonahsaltzman@gmail.com>
2021-12-22 20:16:12 -0500

    move close popup to home from app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,9 @@ import { TWTAUTH_PREFIX, TWTAUTH_SUFFIX } from '../constants'
 const Home = (props) => {
 
 	const location = props.location
+	console.log(`in home, location.search: ${location.search}`)
 	if (location.search === '?close') {
+		console.log('closing window')
 		window.close()
 	}
 
