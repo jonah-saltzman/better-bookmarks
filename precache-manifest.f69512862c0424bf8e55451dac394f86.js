c12475e10841ef2da5ebcf841c750b1efb42569c
jonahsaltzman <jonahsaltzman@gmail.com>
2021-12-23 00:28:40 -0500

    Updates

---
+++ precache-manifest.f69512862c0424bf8e55451dac394f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa55533fbb2a60c268b51c7e80389678",
+    "url": "/better-bookmarks/index.html"
+  },
+  {
+    "revision": "d52dd62b515abcb15cda",
+    "url": "/better-bookmarks/static/css/2.a304e6d6.chunk.css"
+  },
+  {
+    "revision": "d4b9b04efaeb9aa94b23",
+    "url": "/better-bookmarks/static/css/main.57c88517.chunk.css"
+  },
+  {
+    "revision": "d52dd62b515abcb15cda",
+    "url": "/better-bookmarks/static/js/2.c2ac3648.chunk.js"
+  },
+  {
+    "revision": "1a7aa32016436f15b788e3515cd93be1",
+    "url": "/better-bookmarks/static/js/2.c2ac3648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b9b04efaeb9aa94b23",
+    "url": "/better-bookmarks/static/js/main.da4900b1.chunk.js"
+  },
+  {
+    "revision": "b0c1b0507167678b4d2c",
+    "url": "/better-bookmarks/static/js/runtime-main.910ba65b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/better-bookmarks/precache-manifest.ef847da3bd401077cf43405a082f4e97.js"
+  "/better-bookmarks/precache-manifest.f69512862c0424bf8e55451dac394f86.js"
 );
 
 self.addEventListener('message', (event) => {
