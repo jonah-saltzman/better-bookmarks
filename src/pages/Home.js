1784c353631cfa337c438e449f7a0d0054812ea5
jonahsaltzman <jonahsaltzman@gmail.com>
2022-01-05 11:22:12 -0500

    add responsive message if browser prevents window.close

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import { useHistory } from 'react-router-dom'
 
@@ -16,50 +16,72 @@ import { SET_SHARED_FOLDER } from '../context/action.types'
 
 import { AppContext } from '../context/Context'
 
+// open(location, '_self').close()
+
 const Home = (props) => {
 	const { state, dispatch } = useContext(AppContext)
 	const history = useHistory()
 	const { loggedIn } = state
+    const [showClose, setShowClose] = useState(false)
 
-	if (props.location.search === '?close') {
-		window.close()
-	} else if (tokenUrlRE.test(props.location.search)) {
-		const token = props.location.search.match(tokenRE)[0]
-		localStorage.setItem('token', token)
-		window.close()
-	} else if (shareUrlRE.test(props.location.search)) {
-		const share = props.location.search.match(shareRE)[0]
-		dispatch({ type: SET_SHARED_FOLDER, payload: share })
-		history.push('/shared')
-	}
+    function closeWindow() {
+            console.log('clicked close')
+			window.open('', '_parent', '')
+			window.close()
+		}
 
-	fetch(BB_URL)
+    useEffect(() => {
+        if (props.location.search === '?close') {
+                    console.log('attempting to close')
+					window.close()
+					closeWindow()
+					setShowClose(true)
+				} else if (tokenUrlRE.test(props.location.search)) {
+					const token = props.location.search.match(tokenRE)[0]
+					localStorage.setItem('token', token)
+                    console.log('attempting to close')
+					window.close()
+					closeWindow()
+					setShowClose(true)
+				} else if (shareUrlRE.test(props.location.search)) {
+					const share = props.location.search.match(shareRE)[0]
+					dispatch({ type: SET_SHARED_FOLDER, payload: share })
+					history.push('/shared')
+				}
+				fetch(BB_URL)
+    }, [])
+    
+    const stdBody = (
+			<>
+				<ul>
+					<li>Organize your favorite Tweets into folders</li>
+					<li>Share folders with anyone</li>
+					<li>No more "This Tweet has been deleted"</li>
+				</ul>
+				<div className='center'>
+					{loggedIn ? (
+						<img
+							className='center-item'
+							src={twitterLogin}
+							alt='Login with Twitter'
+							style={{ width: '20vw' }}
+							onClick={() => {
+								history.push('/auth')
+							}}
+						/>
+					) : null}
+				</div>
+			</>
+		)
 
     return (
 			<div className='center-home'>
 				<Card className='homecard card-fab'>
 					<CardTitle className='text-large'>
-						Welcome to Better Bookmarks
+						{showClose ? 'Login Succeeded' : 'Welcome to Better Bookmarks'}
 					</CardTitle>
-					<CardBody>
-						<ul>
-							<li>Organize your favorite Tweets into folders</li>
-							<li>Share folders with anyone</li>
-							<li>No more "This Tweet has been deleted"</li>
-						</ul>
-						<div className='center'>
-							{loggedIn ? (
-								<img
-									className='center-item'
-									src={twitterLogin}
-									alt='Login with Twitter'
-									style={{width: '20vw'}}
-									onClick={() => {
-										history.push('/twitter')
-									}}
-								/>
-							) : null}
-						</div>
+					<CardBody onClick={() => closeWindow()}>
+						{showClose ? 'You may return to the app to continue' : stdBody}
 					</CardBody>
 				</Card>
 			</div>
